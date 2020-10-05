88b6d1d556e3e14a816c6f6afe42074707b2cd3f
Angger PW <anggerpenceng10cm@gmail.com>
2020-10-05 21:54:55 +0700

    delete unsupported file

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM node:latest
-
-WORKDIR /src
-COPY package.json /src
-RUN npm install
-COPY . /src
-CMD ["npm", "start"]
--- view/client/application/modules/registers/controllers/Registers.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(!defined('BASEPATH')) exit('No direct script access allowed');
-class Registers extends MX_Controller {
-
-	public function index(){
-		$this->load->view('index');
-	}
-}
-?>
