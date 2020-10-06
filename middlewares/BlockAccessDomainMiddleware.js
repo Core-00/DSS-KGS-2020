ce41ff636e5ef49bdefb2398b8558dd6ff312395
Angger PW <anggerpenceng10cm@gmail.com>
2020-10-06 21:56:10 +0700

    add more server info

--- middlewares/BlockAccessDomainMiddleware.js
+++ middlewares/BlockAccessDomainMiddleware.js
@@ -6,11 +6,12 @@ exports.BlockAccessDomain = async function(req, res, next) {
     var allowedMacAddress = [
         "40:9f:38:e3:36:63", "d4:85:64:6c:e6:e2",
         "d4:85:64:6c:e6:e4" , "d4:85:64:6c:e6:e6",
-        "d4:85:64:6c:e6:e8"
+        "d4:85:64:6c:e6:e8" , "00:50:56:b0:75:2a"
     ]
     var allowedIpServer = [
         "192.168.43.220", "10.10.10.200",
-        "202.53.254.36" , "192.168.18.21"
+        "202.53.254.36" , "192.168.18.21",
+        "202.46.7.26"
     ]
     
     if (
